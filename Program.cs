c363b68283264e516e51b4b1062ab80ef69b326f
Bolseva <bolsevasona@gmail.com>
2024-12-23 10:23:01 +0300

    Загркзка файла

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир!");
+            Console.ReadLine();
+
+        }
+    }
+}
